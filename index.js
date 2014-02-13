45363a51ec59977b2e157c04b29f75151dada820
Thomas Leen <tl@thomasleen.com>
2014-02-13 22:03:57 +0000

    initial tests done

--- index.js
+++ index.js
@@ -1,11 +1,86 @@
 'use strict';
 
-var pkg = require('./package.json');
+var async = require('async'),
+cachy = require('cachy'),
+pkg = require('./package.json'),
+request = require('request'),
+url = require('url'),
+_ = require('underscore');
+
+
+/*
+* store : cachy store
+* defaults : request.js defaults
+*
+*/
 
 module.exports = function(config){
 
+  var configuration = _.defaults({}, config, {
+    store : require('cachy-memory')(),
+    defaults : {}
+  });
+
+  var cache = cachy(configuration.store,_.pick(configuration, 'duration'));
+  var r = request.defaults(configuration.defaults);
+
+
+  var get = function(){ // url, {args-cachable}, {args-uncachable}, callback
+    var args = _.toArray(arguments);
+    var callback = args.pop();
+
+    // deconstruct uri
+    var components = url.parse(args.shift(), true);
+
+    // use initial args as part of url+querystring
+    if(args.length){
+      delete components.search; // force it to build from .query
+      components.query = _.extend(components.query, args.shift());
+    }
+
+    var key = url.format(components);
+    
+    //if cache contains, get and return key'ed data
+
+    // else add any args and call
+    if(args.length){
+      args.unshift(components.query);
+      components.query = _.extend.apply(null, args);
+    }
+
+    var uri = url.format(components);
+//    console.log('key', key);
+//    console.log('url', uri);
+
+    cache.has(key, function(has){
+
+      if(has) return cache.get(key, callback);
+
+      r.get(uri, function(err, response, body){
+	if(err) return callback(err);
+	cache.put(key, body, function(err){
+	  if(err) return callback(err);
+	  return cache.get(key, callback);
+	}); 
+      });
+    });
+  };
+
+  var getJSON = function(){
+    var args = _.toArray(arguments);
+    var callback = args.pop();
+    args.push(function(err, data){
+      if(err) return callback(err);
+      return callback(null, JSON.parse(data));
+    });
+    get.apply(null, args);
+  };
+
   return {
-    version : pkg.version
+    version : pkg.version,
+    clear : cache.clear,
+    get : get,
+    getJSON : getJSON
   };
 
 };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
 'use strict';
 
-var pkg = require('../package.json'),
-should = require('should');
+var async = require('async'),
+pkg = require('../package.json'),
+reflector = require('reflector'),
+should = require('should'),
+_ = require('underscore');
+
+var port = 8080;
+var url = 'http://localhost:' + port + '/';
+
+var server = reflector({port : port});
+before(server.start);
 
 describe('versioning', function(){
   var retainer = require('..')();
@@ -13,3 +22,173 @@ describe('versioning', function(){
     retainer.version.should.be.exactly(pkg.version);
   });
 });
+
+describe('data retreival', function(){  
+  var r = require('..')();
+
+  describe('get', function(){
+    describe('data', function(){
+      it('should return test data', function(done){
+	var testURL = url + '?test=initial-test-data';
+	r.get(testURL, function(err, body){
+	  body.should.be.ok.and.a.String;
+	  var data = JSON.parse(body);
+	  data.should.be.ok.and.a.Object;
+	  data.should.have.property('_meta').and.be.an.Object;
+	  _.omit(data, '_meta').should.eql({'test' : 'initial-test-data'});
+	  return done(err);
+	});
+      });
+    });
+
+    it('should return test data JSON', function(done){
+      var testURL = url + '?test=initial-test-data';
+      r.getJSON(testURL, function(err, json){
+	json.should.be.ok.and.a.Object;
+	json.should.have.property('_meta').and.be.an.Object;
+	_.omit(json, '_meta').should.eql({'test' : 'initial-test-data'});
+	return done(err);
+      });
+    });
+
+    
+    describe('cached data', function(){
+      
+      var data = {number : 100, string : 'foobar is not barfoo', date : (new Date()).toString()};
+      var cachedTime = null;
+      var originalReturnData = null;
+
+      it('should get test data', function(done){
+	r.getJSON(url, data, function(err, json){
+	  json.should.be.ok.and.a.Object;
+	  json.should.have.property('_meta').and.be.an.Object;
+	  _.omit(json, '_meta').should.eql(data);
+	  originalReturnData = json;
+	  return done(err);
+	});
+      });
+
+      it('should get test data from cache', function(done){
+	r.getJSON(url, data, function(err, json){
+	  json.should.eql(originalReturnData);
+	  return done(err);
+	});
+      });
+
+      it('should get test data from cache with extra request args', function(done){
+	r.getJSON(url, data, {another : 'argument'}, function(err, json){
+	  json.should.eql(originalReturnData);
+	  return done(err);
+	});
+      });
+    });
+  });
+});
+
+describe('timed cache', function(){  
+  var duration = 5000; // ms
+  var r = require('..')({
+    duration : duration
+  });
+
+
+  var testURL = (url + 'timed-cache/test/');
+  var originalData = {title : 'this is the first data test', date : (new Date()).toString()};
+  var originalReturnData = null;
+  
+  it('should load data into cache', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object;
+      json.should.have.property('_meta').and.be.an.Object;
+      _.omit(json, '_meta').should.eql(originalData);
+      originalReturnData = json;
+      return done(err);
+    });
+  });
+
+  it('should wait for less than duration', function(done){
+    this.timeout(duration);
+    setTimeout(done, duration / 5);
+  });
+
+  it('should still have the same data', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object.and.eql(originalReturnData);
+      json._meta.count.should.be.exactly(1);
+      return done(err);
+    });
+  });
+
+  it('should wait for less than duration', function(done){
+    this.timeout(duration);
+    setTimeout(done, duration / 5);
+  });
+
+  it('should still have the same data', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object.and.eql(originalReturnData);
+      json._meta.count.should.be.exactly(1);
+      return done(err);
+    });
+  });
+
+  it('should still have the same data with extra request args', function(done){
+    r.getJSON(testURL, originalData, {extradata : 'Should not be taken into account'}, function(err, json){
+      json.should.be.ok.and.a.Object.and.eql(originalReturnData);
+      json._meta.count.should.be.exactly(1);
+      return done(err);
+    });
+  });
+
+
+  it('should wait for duration', function(done){
+    this.timeout(duration * 2);
+    setTimeout(done, duration);
+  });
+
+  var nextReturnData = null;
+  it('should have refreshed data', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object.and.not.eql(originalReturnData);
+      json._meta.count.should.be.exactly(2);
+      nextReturnData = json;
+      return done(err);
+    });
+  });
+
+  it('should still have the refreshed data', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object.and.eql(nextReturnData);
+      return done(err);
+    });
+  });
+
+  it('should wait for duration', function(done){
+    this.timeout(duration * 2);
+    setTimeout(done, duration);
+  });
+
+  it('should have refreshed data', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object.and.not.eql(nextReturnData);
+      json._meta.count.should.be.exactly(3);
+      return done(err);
+    });
+  });
+
+  it('should clear', function(done){
+    r.clear(done);
+  });
+
+  it('should have new data', function(done){
+    r.getJSON(testURL, originalData, function(err, json){
+      json.should.be.ok.and.a.Object;
+      json._meta.count.should.be.exactly(4);
+      return done(err);
+    });
+  });
+
+
+});
+
+after(server.stop);
